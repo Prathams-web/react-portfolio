fcedbad4d238f3862e65848a74d48c9ff4197705
Prathamesh Tirlotkar <prathameshtirlotkar2@gmail.com>
2024-03-31 20:31:28 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import aboutImg from '../assets/about.jpg'  
+import { ABOUT_TEXT } from '../constants'
+import { motion } from "framer-motion"
+
+
+function About() {
+  return (
+    <div className='border-b border-neutral-900 pb-4'>
+        <h2 className='my-20 text-center text-4xl'>
+            Resume
+            {/* <span className='text-neutral-500'> Me</span> */}
+        </h2>
+        <div className='flex flex-wrap justify-center'>
+            <motion.div 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 0.5 }}
+                className='w-full lg:w-2/3 lg:p-8'
+            >
+                <div className='flex items-center justify-center'>
+                    <img className='rounded-2xl' src="https://res.cloudinary.com/dmj5vxvtq/image/upload/v1711892013/Resume/Prathamesh_Resume_New_t7k0ma_-_Portrait_Post_yl9j3y.png" alt="about" />
+                </div>
+            </motion.div>
+            <motion.div 
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 0.5 }}
+                className='w-full lg:w-2/3 '
+            >
+                {/* <div className='flex justify-center '>
+                    <p className='my-2 max-w-xl py-6 text-center'>{ABOUT_TEXT}</p>
+                </div> */}
+            </motion.div>    
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../assets/kevinRushLogo.png';
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import { FaInstagram } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex justify-between items-center py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        {/* <img className='mx-2 w-10' src={logo} alt="logo" /> */}
+      </div>
+      <div className='m-8 flex justify-center items-center gap-4 text-2xl'>
+        <a href="https://www.linkedin.com/in/prathamesh-tirlotkar" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href="https://github.com/Prathams-web" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="#"><FaInstagram /></a>
+        <a href="#"><FaSquareXTwitter /></a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
